e5a195850b0944054c40c7bc4d5b9eb36abc0d38
alanhon125 <78402525+alanhon125@users.noreply.github.com>
2024-08-02 15:19:06 -0700

    Delete dev_scripts directory

--- dev_scripts/nohup_runserver.bash
+++
@@ -1,8 +0,0 @@
-kill -9 $(lsof -t -i:8000)
-# calling conda source activate boc from bash script
-eval "$(conda shell.bash hook)"
-source activate
-conda activate boc
-cd /home/data/ldrs_analytics
-rm /home/data/ldrs_analytics/data/log/syslog_port8000.log
-nohup python manage.py runserver 0.0.0.0:8000 > data/log/syslog_port8000.log 2>&1 &
